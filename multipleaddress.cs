21ad0aa6ae2af39611b2e57b57e8d0a3e157bd97
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 09:15:53 +0530

    adding multiple addressbook

---
+++ multipleaddress.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbooksystem
+{
+    internal class multipleaddress
+    {
+        public address find(string name, object addresses)
+        {
+            address addr = addresses.Find( name);
+            return addr;
+        }
+    }
+}
