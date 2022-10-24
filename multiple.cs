0c7d021b1f254050c1faad2d731772de4212beb5
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 09:14:04 +0530

    adding multiple person

---
+++ multiple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbooksystem
+{
+    internal class multiple
+    {
+        private readonly object addresses;
+
+        public void list(Action<multipleaddress> action)
+        {
+            addresses.ForEach(action);
+        }
+
+        public bool isEmpty()
+        {
+            addresses.Count == 0;
+        }
+    }
+}
