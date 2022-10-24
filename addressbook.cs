dfef295fc934afedc9b9bb26eebde66d7b8babd6
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 09:09:04 +0530

    adding new details

---
+++ addressbook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbooksystem
+{
+    public class address
+    {
+
+        List<address> addresses;
+
+        public address()
+        {
+            addresses = new List<address>();
+        }
+
+        internal static void Address()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
