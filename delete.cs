c768ff45199a7f6e78b171cf6c6d9069cddf3c9c
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-24 09:12:30 +0530

    delete person using name

---
+++ delete.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbooksystem
+{
+    internal class delete
+    {
+        public bool remove(string name, object addresses)
+        {
+            address addr = find(name);
+
+            if (addr != null)
+            {
+                  addresses.Remove(addr);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private address find(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
